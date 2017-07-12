fe082de4c95ca3745dd9e560b550b183b3a38328
Dmitry Shechtman <damnian@gmail.com>
2017-07-12 11:19:27 +0300

    Replace projectName with categoryName

--- src/Net.Chdk.Meta.Providers.Software.Core/ISoftwareMetaProvider.cs
+++ src/Net.Chdk.Meta.Providers.Software.Core/ISoftwareMetaProvider.cs
@@ -5,6 +5,6 @@ namespace Net.Chdk.Meta.Providers.Software
 {
     public interface ISoftwareMetaProvider
     {
-        IEnumerable<SoftwareInfo> GetSoftware(string path, string productName);
+        IEnumerable<SoftwareInfo> GetSoftware(string path, string categoryName);
     }
 }
