befe9b7c8ccc94579ce07834e044c2025359f4b6
Dmitry Shechtman <damnian@gmail.com>
2017-07-16 06:02:12 +0300

    categoryName -> productName

--- src/Net.Chdk.Meta.Providers.Software.Core/ISoftwareMetaProvider.cs
+++ src/Net.Chdk.Meta.Providers.Software.Core/ISoftwareMetaProvider.cs
@@ -5,6 +5,6 @@ namespace Net.Chdk.Meta.Providers.Software
 {
     public interface ISoftwareMetaProvider
     {
-        IEnumerable<SoftwareInfo> GetSoftware(string path, string categoryName);
+        IEnumerable<SoftwareInfo> GetSoftware(string path, string productName);
     }
 }
